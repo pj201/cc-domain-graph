508c6bac12dbe225901b417b6429c5d4ba2f2326
Bill Ox <bill@Fidelio.local>
2017-09-03 12:05:10 +0100

    set up on EMR

--- setup.sh
+++ setup.sh
@@ -6,21 +6,18 @@ sudo yum -y install pssh
 sudo pip install -e git+https://github.com/commoncrawl/gzipstream.git#egg=gzipstream
 sudo pip install warc ujson
 
+git clone https://github.com/box121209/cc-domain-graph
+
 #export SPARK_HOME=/usr/lib/spark
 #export PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python:$SPARK_HOME/python/lib
-
 #sudo ln -s $SPARK_HOME /usr/local
-
 #sudo pip install jupyter
-
 #export PYSPARK_DRIVER_PYTHON=jupyter
 #export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
-
 #aws s3 cp s3://aws-bigdata-blog/artifacts/aws-blog-emr-jupyter/install-jupyter-emr5.sh .
 #chmod +x install-jupyter-emr5.sh
 # the next bit takes quite a few minutes:
 #./install-jupyter-emr5.sh —-toree
-
 #sudo pip install toree
 #sudo jupyter toree install --interpreters=PySpark,SQL
 
@@ -50,9 +47,9 @@ pssh -h ./slaves 'sudo yum -y install git; sudo yum -y install pssh; sudo pip in
 
 
 #################################################################
-# start up pyspark on port 8889
+# move to cc-domain-graph directory
 
-#pyspark &
+cd cc-*
 
 #################################################################
 
