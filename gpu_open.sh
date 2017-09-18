dcb14c5b05f348f96a0708134e63a5b98694e9c8
Bill Ox <box121209@gmail.com>
2017-09-18 14:57:51 +0100

    Added cuDNN library

--- gpu_open.sh
+++ gpu_open.sh
@@ -3,8 +3,8 @@ KEY="/Users/49269/.ssh/Billsdata-20170604.pem"
 
 [ $# -eq 0 ] && { echo "Usage: $0 AWS_Hostname"; exit 1; }
 
-# Copy setup script to master node
+# Copy setup script to EC2 instance:
 scp -i $KEY ./gpu_setup.sh ubuntu@$1:~
 
-# Login to master, with tunnel for Jupyter
+# Login to master, with tunnels for Jupyter and TensorBoard:
 ssh -i $KEY -L 8888:localhost:8888 -L 6006:localhost:6006 ubuntu@$1
--- gpu_setup.sh
+++ gpu_setup.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
+# EC2 GPU system prep for deep learning
 
 # See:
-# https://github.com/fluxcapacitor/pipeline/wiki/AWS-GPU-TensorFlow-Docker
-# https://medium.com/@acrosson/installing-nvidia-cuda-cudnn-tensorflow-and-keras-69bbf33dce8a
+# https://www.tensorflow.org/install/install_linux
 #
 # Choose Ubuntu 16.04 GPU p2.xlarge (Tesla K80)
+# CUDA Toolkit 8.0
+# cuDNN 7.0 (or 6.0 if find problems)
+# Tensorflow, Keras, Jupyter
+# Install with Virtualenv
 
 # check GPU
 
@@ -13,33 +16,37 @@ lspci -nnk | grep -i nvidia
 # install basics
 
 sudo apt-get update
-sudo apt-get install libglu1-mesa libxi-dev libxmu-dev -y
-sudo apt-get — yes install build-essential
+#sudo apt-get install libglu1-mesa libxi-dev libxmu-dev -y
+#sudo apt-get — yes install build-essential
 sudo apt-get install python-pip python-dev -y
 sudo apt-get install python-numpy python-scipy -y
 sudo apt-get install -y linux-headers-$(uname -r)
 sudo apt-get install -y gcc
 sudo apt-get install -y make
 sudo apt-get install -y g++
+sudo apt-get install git
 
 # clone git repo
 
-sudo apt-get install git
 git clone https://github.com/box121209/cc-domain-graph
 
 # CUDA drivers and toolkit
 # see
-# http://www.nvidia.com/Download/index.aspx?lang=en-us
+# http://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html
+
+mkdir packages
+cd packages
 
 wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
 sudo dpkg -i cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
 sudo apt-get update
 sudo apt-get install -y cuda
-export PATH=$PATH:/usr/local/cuda/bin
+export PATH=$PATH:/usr/local/cuda-8.0/bin
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64\
+                         ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 # verify installation
 
-echo '---> testing...'
 nvidia-smi
 nvcc --version
 sudo modprobe nvidia
@@ -49,45 +56,46 @@ sudo make
 cd 
 
 # install cuDNN
+# See
+# http://docs.nvidia.com/deeplearning/sdk/cudnn-install/index.html
+
+cp ~/cc-domain-graph/lib/libcudnn7_7.0.2.38-1+cuda8.0_amd64.deb .
+
+sudo dpkg -i libcudnn7_7.0.2.38-1+cuda8.0_amd64.deb
 
 
+sudo apt-get install libcupti-dev
+
+# install tensorflow using Virtualenv
+# see
+# https://www.tensorflow.org/install/install_linux#nvidia_requirements_to_run_tensorflow_with_gpu_support
+
+sudo apt-get install libcupti-dev
+
+sudo apt-get install python-pip python-dev python-virtualenv # for Python 2.7
+
+mkdir ~/tensorflow
+sudo virtualenv --system-site-packages ~/tensorflow # for Python 2.7
+
+source ~/tensorflow/bin/activate
+
+# EITHER
+
+easy_install -U pip
+
+# OR (if easy_install fails)
+
+sudo pip install --upgrade tensorflow      # for Python 2.7
+sudo pip install --upgrade tensorflow-gpu  # for Python 2.7 and GPU
+sudo pip install --upgrade <tfBinaryURL>
 
 
-##########################################################
-# set up Docker
+# install jupyter and keras etc
 
 sudo apt-get update
-sudo curl -fsSL https://get.docker.com/ | sh
-sudo curl -fsSL https://get.docker.com/gpg | sudo apt-key add -
-
-# set up NVidia Docker instance
-
-wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
-sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
-
-# test NVidia SMI
-
-sudo nvidia-docker run --rm nvidia/cuda nvidia-smi
-
-# download Docker image with TF examples
-#sudo docker pull fluxcapacitor/gpu-tensorflow-spark:master
-#sudo nvidia-docker run -itd --name=gpu-tensorflow-spark -p 80:80 -p 50070:50070 -p 39000:39000 -p #9000:9000 -p 9001:9001 -p 9002:9002 -p 9003:9003 -p 9004:9004 -p 6006:6006 -p 8754:8754 -p #7077:7077 -p 6066:6066 -p 6060:6060 -p 6061:6061 -p 4040:4040 -p 4041:4041 -p 4042:4042 -p #4043:4043 -p 4044:4044 -p 2222:2222 -p 5050:5050 fluxcapacitor/gpu-tensorflow-spark:master
-# verify successful set-up
-#sudo nvidia-docker exec -it gpu-tensorflow-spark bash
-#nvidia-smi
-#exit # to return to ubuntu
-# run tensorboard in the background
-#tensorboard --logdir /root/train &
-
-# install jupyter and keras
-
-sudo apt-get update
-sudo apt-get -y install python-pip
 sudo apt-get -y install ipython ipython-notebook
 sudo pip install --upgrade pip
 sudo pip install jupyter
-sudo pip install tensorflow
-sudo pip install tensorflow-gpu
 sudo pip install keras
 sudo pip install boto
 
